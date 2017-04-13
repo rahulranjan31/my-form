3949dae987866f29e0b870fc70d90b6289b47fd8
Rahul Ranjan <ranjan.rahul326@gmail.com>
2017-04-13 14:22:46 +0530

    model driven form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,22 @@
 import { Component } from '@angular/core';
-
+import { FormGroup, FormControl } from '@angular/forms';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styles: [`input.ng-invalid{border-left: 5px solid red;}
-    input.ng-valid{border-left: 5px solid green}`]
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  //myName = "Rahul";
-  onSubmit(value: any) {
-    console.log(value);
-
+  userForm = new FormGroup({
+    name: new FormControl(),
+    email: new FormControl(),
+    contact: new FormControl(),
+    address: new FormGroup({
+      street: new FormControl(),
+      city: new FormControl(),
+      pincode: new FormControl()
+    })
+  });
+  onSubmit() {
+    console.log(this.userForm.value);
   }
 }
