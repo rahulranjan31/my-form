f2f10c43005a06a08a1c36ffc44ae648d1f354fb
Rahul Ranjan <ranjan.rahul326@gmail.com>
2017-04-13 11:18:05 +0530

    template driven form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styles: [`input.ng-invalid{border-left: 5px solid red;}
+    input.ng-valid{border-left: 5px solid green}`]
 })
 export class AppComponent {
-  title = 'app works!';
+  //myName = "Rahul";
+  onSubmit(value: any) {
+    console.log(value);
+
+  }
 }
